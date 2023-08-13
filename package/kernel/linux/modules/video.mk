a4f8fc9fb4d1320650d9f624d02b520e14386691
Tomasz Maciej Nowak <tmn505@gmail.com>
2023-08-13 18:21:13 +0200

    tegra: cortexa9: enable VDE driver
    This drives power domain responsible for clean reboot.
    
    Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -1146,7 +1146,7 @@ define KernelPackage/video-mem2mem
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Memory 2 Memory device support
   HIDDEN:=1
-  DEPENDS:=+kmod-video-videobuf2
+  DEPENDS:=@!TARGET_tegra_cortexa9 +kmod-video-videobuf2
   KCONFIG:= \
     CONFIG_V4L_MEM2MEM_DRIVERS=y \
     CONFIG_V4L2_MEM2MEM_DEV
@@ -1165,7 +1165,7 @@ define KernelPackage/video-dma
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Video DMA support
   HIDDEN:=1
-  DEPENDS:=+kmod-video-videobuf2
+  DEPENDS:=@!TARGET_tegra_cortexa9 +kmod-video-videobuf2
   KCONFIG:= \
 	CONFIG_VIDEOBUF2_DMA_CONTIG \
 	CONFIG_VIDEOBUF2_DMA_SG
