fa353205027e9ef7819046c58280a8e6b0d23a90
Maksim <8supermaks8@gmail.com>
2024-08-30 02:21:17 +0300

    fix

--- make.sh
+++ make.sh
@@ -6,6 +6,10 @@ make clean
 #make distclean
 #./scripts/feeds update -f nss && ./scripts/feeds install -a -p nss && \
 ./scripts/feeds update -a && \
+
+# Apply curl_wolfssl_quic_tls13.patch
+(patch --verbose -p0 -N < ./build-fixes/curl_wolfssl_quic_tls13.patch && (./scripts/feeds update -a && cp bak5_zapret.config .config))
+
 cp build-fixes/feeds.nss.qca-nss-clients.Makefile feeds/nss/qca-nss-clients/Makefile && \
 cp build-fixes/feeds.nss.qca-nss-ecm.Makefile feeds/nss/qca-nss-ecm/Makefile && \
 cp build-fixes/package_feeds_luci_luci-mod-status_patches_990-add-nss-load-to-status.patch package/feeds/luci/luci-mod-status/patches/990-add-nss-load-to-status.patch && \
@@ -13,7 +17,6 @@ cp build-fixes/package_feeds_luci_luci-mod-status_patches_990-add-nss-load-to-st
 #cp build-fixes/target.linux.ipq806x.patches-5.15.999-998-bbr-plus-tsq.patch target/linux/ipq806x/patches-5.15/999-998-bbr-plus-tsq.patch && \
 #git apply build-fixes\930-bbr-plus-tsq.patch && \
 #cp build-fixes/package.network.services.dnsmasq.patches.900-strict-order-nxdomain.patch package/network/services/dnsmasq/patches/900-strict-order-nxdomain.patch && \
-patch --verbose -p0 < ./build-fixes/curl_wolfssl_quic_tls13.patch && \
 ./scripts/feeds install -a && \
 #cp diff_base .config && make defconfig && \
 ./scripts/getver.sh && \
