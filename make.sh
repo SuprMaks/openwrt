d8bd0300388b5cb205bcfbc43afdd3a1ef688121
Maksim <8supermaks8@gmail.com>
2024-09-04 14:59:37 +0300

    Upd

---
+++ make.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#make target/compile package/{qca-nss-gmac,qca-nss-drv,qca-nss-clients,qca-nss-ecm}/{clean,compile} V=s -j10
+#cp build-fixes/feeds.nss.qca-nss-clients.Makefile feeds/nss/qca-nss-clients/Makefile
+#cp build-fixes/feeds.nss.qca-nss-ecm.Makefile feeds/nss/qca-nss-ecm/Makefile
+make clean
+#make distclean
+#./scripts/feeds update -f nss && ./scripts/feeds install -a -p nss && \
+./scripts/feeds update -a && \
+
+# Apply curl_wolfssl_quic_tls13.patch
+(patch --verbose -p0 -N < ./build-fixes/curl_wolfssl_quic_tls13.patch && (./scripts/feeds update -a && cp bak_def_jetson.config .config))
+
+#cp build-fixes/feeds.nss.qca-nss-clients.Makefile feeds/nss/qca-nss-clients/Makefile && \
+#cp build-fixes/feeds.nss.qca-nss-ecm.Makefile feeds/nss/qca-nss-ecm/Makefile && \
+#cp build-fixes/package_feeds_luci_luci-mod-status_patches_990-add-nss-load-to-status.patch package/feeds/luci/luci-mod-status/patches/990-add-nss-load-to-status.patch && \
+
+#cp build-fixes/target.linux.ipq806x.patches-5.15.999-998-bbr-plus-tsq.patch target/linux/ipq806x/patches-5.15/999-998-bbr-plus-tsq.patch && \
+#git apply build-fixes\930-bbr-plus-tsq.patch && \
+#cp build-fixes/package.network.services.dnsmasq.patches.900-strict-order-nxdomain.patch package/network/services/dnsmasq/patches/900-strict-order-nxdomain.patch && \
+./scripts/feeds install -a && \
+#cp diff_base .config && make defconfig && \
+./scripts/getver.sh && \
+make -j $(nproc) download world
