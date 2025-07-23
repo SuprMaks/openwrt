70b502e9e35966f9ec932184a5db10595e6bc11a
Maksim Kapustin <8supermaks8@gmail.com>
2025-07-23 11:16:21 +0300

    Upd

--- make.sh
+++ make.sh
@@ -2,8 +2,15 @@
 
 source $(dirname -- "$0")/build-fixes/config.sh
 
-make defconfig dirclean && \
-#make defconfig clean && \
+[[ -d build_dir/malformed_packages ]] && rm -rf build_dir/malformed_packages
+mkdir -p ./build_dir/malformed_packages
+git clone https://github.com/gSpotx2f/luci-app-temp-status.git build_dir/malformed_packages/luci-app-temp-status && \
+git clone https://github.com/gSpotx2f/luci-app-cpu-perf.git build_dir/malformed_packages/luci-app-cpu-perf && \
+git clone https://github.com/gSpotx2f/luci-app-cpu-status.git build_dir/malformed_packages/luci-app-cpu-status && \
+#git clone https://github.com/muink/luci-app-natmapt.git build_dir/malformed_packages/luci-app-natmapt && \
+
+#make defconfig dirclean && \
+make defconfig clean && \
 ./scripts/feeds clean
 make defconfig && \
 
@@ -15,7 +22,13 @@ make defconfig && \
 # Apply curl_wolfssl_quic_tls13.patch
 patch --verbose -p0 -N < ./build-fixes/curl_wolfssl_quic_tls13.patch && ./scripts/feeds update -a
 
-./scripts/feeds install -a && summarize_conf && make defconfig && \
+./scripts/feeds install -a && \
+
+rm -rf feeds/packages/lang/golang && \
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang && \
+
+summarize_conf && \
+#make defconfig && \
 
 #cp build-fixes/target.linux.ipq806x.patches-5.15.999-998-bbr-plus-tsq.patch target/linux/ipq806x/patches-5.15/999-998-bbr-plus-tsq.patch && \
 #git apply build-fixes\930-bbr-plus-tsq.patch && \
@@ -23,7 +36,7 @@ patch --verbose -p0 -N < ./build-fixes/curl_wolfssl_quic_tls13.patch && ./script
 
 #make clean && \
 
-./scripts/feeds install -a && summarize_conf && \
+#./scripts/feeds install -a && summarize_conf && \
 
 #cp diff_base .config && make defconfig && \
 #cp build-fixes/bak.config .config && \
@@ -37,6 +50,7 @@ make defconfig menuconfig && \
 #echo 'CONFIG_TARGET_SUFFIX="muslgnueabihf"' >> .config && \
 sed -i 's/CONFIG_TARGET_SUFFIX="muslgnueabi"/CONFIG_TARGET_SUFFIX="muslgnueabihf"/' .config && \
 #sed -i '/--disable-multilib \\/d' ./toolchain/gcc/common.mk && \
+grep -q 'CONFIG_ATH10K_LEDS=y' .config || \
 make -j16 -l $(nproc) download world
 
 #make ./scripts/config/conf >/dev/null || { make ./scripts/config/conf; exit 1; } && \
--- target/linux/tegra/cortexa57/target.mk
+++ target/linux/tegra/cortexa57/target.mk
@@ -1,7 +1,10 @@
 ARCH:=aarch64
 BOARDNAME:=Tegra with Cortex-A57
 CPU_TYPE:=cortex-a57
+CPU_SUBTYPE:=neon-vfpv4
 KERNELNAME:=Image dtbs
+
+FEATURES+=arm64_erratum_1319367 arm64_erratum_832075
 
 define Target/Description
         Build firmware images for nVIDIA Tegra (Cortex-A57) based boards.
